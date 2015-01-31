90e263406cf62335ddb9978cce898b450f1d35f4
Paul Rickard <paul@impulseair.com.au>
2015-01-31 12:14:41 +1100

    Separate JPEG functions

---
+++ PHPjpeg-extract.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+
+PHPjpeg-extract based on extract-files by mjb2k -> https://github.com/mjb2k/extract-files
+
+Usage: 
+
+*/
+
+
+
+
+//////////////////////////////////////// Variables ////////////////////////////////////////
+
+//File buffer size
+$readbuffer = 1024 * 1024 *10;
+
+// Default Output directory
+$output_dir = "./";
+
+// File name override
+$file_name = null;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+?>
+
