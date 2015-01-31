11e1e7f3f63492ba3d246f01aa3b3934deb9382f
Paul Rickard <paul@impulseair.com.au>
2015-01-31 17:40:41 +1100

    31-1-15 5:40pm

--- PHPjpeg-extract.php
+++ PHPjpeg-extract.php
@@ -2,7 +2,7 @@
 
 /*
 
-PHPjpeg-extract based on extract-files by mjb2k -> https://github.com/mjb2k/extract-files
+PHPjpegExtract based on extract-files by mjb2k -> https://github.com/mjb2k/extract-files
 
 Usage: 
 
@@ -11,21 +11,31 @@ Usage:
 
 
 
-//////////////////////////////////////// Variables ////////////////////////////////////////
+function PHPjpegExtract($filename, $dir="./", $fileprefix=date('U'), $outdir=$filename){
+	
+	// CHeck for NULL filename
+	if(is_null($filename)) {
+		echo "Error: No Input File Specified";
+		return;
+	}
 
-//File buffer size
-$readbuffer = 1024 * 1024 *10;
+	
+	// JPEG file structure variables
+	$headers = unpack("C*", "\xC0\xC2\xC4\xDB\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xFE\xDD");
+	$RSTn = unpack("C*", "\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7");
+ 	$MARKER = 0xFF;
+ 	$SOI = 0xD8;
+ 	$SOS = 0xDA;
+ 	$EOI = 0xD9;
+ 	$NULL = 0x00;
 
-// Default Output directory
-$output_dir = "./";
+ 	// read file
+ 	$handle = fopen($filename, "rb");
+ 	$contents = fread($handle, filesize($filename));
+ 	fclose($handle);
 
-// File name override
-$file_name = null;
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-
-
+ 	
+}
 
 
 ?>
